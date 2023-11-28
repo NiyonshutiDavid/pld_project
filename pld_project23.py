0ce6bf747d5ab157bc5b98d840e157f8b3566c79
jumulisa <j.umulisa@alustudent.com>
2023-11-28 15:03:50 +0200

    jolly's commit

--- pld_project23.py
+++ pld_project23.py
@@ -1,6 +1,9 @@
 import re
 
 def welcome_message():
+    """
+    This will be the message the user will see when logged in.
+    """
     return "Welcome to the Entrepreneurship Incubator! Choose an option below to get started:"
 
 def main_menu():
